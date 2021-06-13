a07da7c2e934cf80f15228fc572d104d0d4958b1
yoshi0518 <nakamura@n-asset.com>
2021-06-13 22:40:24 +0900

    [add]v1サブアプリケーションを追加

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 
 from config import config
+from v1.main import app as v1_app
 
 app = FastAPI()
+
+app.mount("/v1", v1_app)
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "main root"}
 
 
 if __name__ == "__main__":
