7426e43c2b210a5b473b62bc8495716c5dcc24c5
yoshi0518 <nakamura@n-asset.com>
2021-06-14 05:59:42 +0900

    [add]CORS設定を追加

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from config import config
 from v1.main import app as v1_app
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.mount("/v1", v1_app)
 
