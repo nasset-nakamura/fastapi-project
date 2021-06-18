f567e22bf794b4d961a54ec051ceb302e7699a50
nasset-nakamura <nakamura@n-asset.com>
2021-06-18 10:48:47 +0900

    [update]Userモデルクラスにtitle・validation・exampleを追加

--- v1/schemas/user.py
+++ v1/schemas/user.py
@@ -1,8 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class User(BaseModel):
-    name: str
-    username: Optional[str] = None
-    email: str
+    name: str = Field(
+        ...,
+        title="名前",
+        min_length=1,
+        max_length=100,
+    )
+    username: Optional[str] = Field(
+        None,
+        title="ユーザー名",
+        min_length=1,
+        max_length=100,
+    )
+    email: str = Field(
+        ...,
+        title="メールアドレス",
+        min_length=1,
+        max_length=100,
+    )
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "test",
+                "username": "testuser",
+                "email": "test@example.com",
+            }
+        }
