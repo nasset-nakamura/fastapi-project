095cb108e77b4cc0496ff246b1dc53eb8d68f848
nasset-nakamura <nakamura@n-asset.com>
2021-06-22 16:51:53 +0900

    [add]カスタムヘッダー返却機能を追加

--- v1/routers/others.py
+++ v1/routers/others.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import time
 
 from fastapi import APIRouter, BackgroundTasks, Query, Request, Response
+from fastapi.responses import JSONResponse
 
 from ..utils import logging
 
@@ -45,3 +46,20 @@ async def read_cookies(request: Request):
 async def create_cookies(response: Response):
     response.set_cookie(key="test", value="fastapi cookie")
     return {"Save cookies with fastapi"}
+
+
+@router.get("/header1")
+async def read_header_1(response: Response):
+    response.headers["X-Custom-String"] = "custom string"
+    response.headers["X-Custom-Count"] = "100"
+    return {"Return custom header"}
+
+
+@router.get("/header2")
+async def read_header_2():
+    headers = {
+        "X-Custom-String": "custom string",
+        "X-Custom-Count": "100",
+    }
+    content = "Return custom header"
+    return JSONResponse(headers=headers, content=content)
